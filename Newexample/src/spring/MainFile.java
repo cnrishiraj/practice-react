deff17adb239d0ce955964e9727a641acaf0b410
rishiraj <cnrishiraj@gmail.com>
2020-11-06 12:37:18 +0530

    spring_1

--- Newexample/src/spring/MainFile.java
+++ Newexample/src/spring/MainFile.java
@@ -2,19 +2,19 @@ package spring;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+
 
 public class MainFile {
 	public static void main(String[] args) {
 		
-		Resource resource=new ClassPathResource("applicationContext.xml");
-		BeanFactory factory=new XmlBeanFactory(resource);
-		
-		
-		Employee e1=(Employee)factory.getBean("empBean");
+		ApplicationContext context =new ClassPathXmlApplicationContext("applicationContext.xml");
+		//BeanFactory factory=new XmlBeanFactory(resource);
+		Employee e1=(Employee)context.getBean("empBean");
 		System.out.println(e1);
-		
 		
 	}
 
--- Newexample/src/spring/sample.java
+++
@@ -1,7 +0,0 @@
-package spring;
-
-public class sample {
-	public static void main(String args[]) {
-		System.out.println("hello this is ubuntu");
-	}
-}
