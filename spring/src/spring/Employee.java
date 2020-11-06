6f21582b66a26d60fa735023750e875e791a17b4
rishiraj <cnrishiraj@gmail.com>
2020-11-06 13:02:19 +0530

    inheritance

--- spring/src/spring/Employee.java
+++ spring/src/spring/Employee.java
@@ -1,52 +1,70 @@
 package spring;
 
+import java.util.ArrayList;
+
 public class Employee {
-	
 	private Integer empId;
 	private String name;
 	private double salary;
-
-	public Employee() {
-		System.out.println("Default constructor of Employee is called");
-		empId=0;
-		name="";
-		salary=0;
-	}
+	private Address address;
+	private ArrayList<String> technologies;
 	
-	public Employee(Integer empId, String name, double salary) {
-		System.out.println("Parameterised constructor of Employee is called");
+
+	private ArrayList<Projects> projects;
+	public Employee() {
+		super();
+	}
+	public Employee(Integer empId, String name, double salary,Address address,ArrayList<String> technologies,ArrayList<Projects> projects) {
+		super();
 		this.empId = empId;
 		this.name = name;
 		this.salary = salary;
+		this.address=address;
+		this.technologies=technologies;
+		this.projects=projects;
 	}
-	
+	public ArrayList<Projects> getProjects() {
+		return projects;
+	}
+	public void setProjects(ArrayList<Projects> projects) {
+		this.projects = projects;
+	}
+	public ArrayList<String> getTechnologies() {
+		return technologies;
+	}
+	public void setTechnologies(ArrayList<String> technologies) {
+		this.technologies = technologies;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 	public Integer getEmpId() {
 		return empId;
 	}
-	
 	public void setEmpId(Integer empId) {
 		this.empId = empId;
 	}
-	
 	public String getName() {
 		return name;
 	}
-	
 	public void setName(String name) {
 		this.name = name;
 	}
-	
 	public double getSalary() {
 		return salary;
 	}
-	
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
-	
 	@Override
 	public String toString() {
-		return "Employee [empId=" + empId + ", name=" + name + ", salary=" + salary + "]";
+		return "Employee [empId=" + empId + ", name=" + name + ", salary=" + salary + ", address=" + address
+				+ ", technologies=" + technologies + ", projects=" + projects + "]";
 	}
-
+	
+	
+	
 }
