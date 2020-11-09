2c3279b7bfe06bd0480579121918bb0eb42f98fb
rishiraj <cnrishiraj@gmail.com>
2020-11-09 10:38:05 +0530

    spring_custom_event_handler

---
+++ spring_custom_event_handling_example/src/com/training/org/CustomEvent.java
@@ -0,0 +1,17 @@
+package com.training.org;
+
+import org.springframework.context.ApplicationEvent;
+
+public class CustomEvent extends ApplicationEvent{
+   
+   public CustomEvent(Object source) {
+      super(source);
+   }
+
+   public String toString(){
+      return "My Custom Event";
+   }
+}
+
+
+
